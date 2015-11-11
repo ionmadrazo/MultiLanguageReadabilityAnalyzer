ca67de699ad7359a3151e1cfe6ee4a883b7b016d
Ion Madrazo <ionmadrazo@boisestate.edu>
2015-11-11 00:19:30 -0700

    Creating dev datasets. Part of Speech tag feature added.

---
+++ datasets/readme.txt
@@ -0,0 +1,3 @@
+#####english_cambridge_4_lvls#####
+B1, B2, C1, C2 Cambridge exams, about 5 readings per level
+http://www.flo-joe.co.uk/exams.htm
--- src/constants/TagKeys.java
+++ src/constants/TagKeys.java
@@ -6,7 +6,6 @@ public class TagKeys {
 	public static final String TAG_LANGUAGE = "TAG_LANGUAGE";
 	public static final String TAG_FOLDER = "TAG_FOLDER";
 	public static final String TAG_FILENAME = "TAG_FILENAME";
-	public static final String TAG_LEMMA_CHARLENGTH_AVERAGE = "TAG_LEMMA_CHAR_LENGTH_AVERAGE";
-	
-	
+	public static final String TAG_LEMMA_CHARLENGTH_AVERAGE = "TAG_LEMMA_CHAR_LENGTH_AVERAGE";	
+	public static final String TAG_POS_PREFIX = "TAG_POS_";
 }
